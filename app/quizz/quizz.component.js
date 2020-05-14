905316eabd76483df59840fdb64ae72adbd527ac
AdrienAutcq <adrien.autricque@gmail.com>
2020-05-14 17:04:08 +0200

    quizz improvement

--- app/quizz/quizz.component.js
+++ app/quizz/quizz.component.js
@@ -4,84 +4,185 @@ angular.
   module('quizz').
   component('quizz', {
     templateUrl: 'quizz/quizz.template.html',
-    controller: ['movie', '$scope',
-      function quizzController(movie, $scope) {
+    controller: ['movie', '$scope', 'category', '$http', 'actor',
+      function quizzController(movie, $scope, category, $http, actor) {
         
         var self = this;
+
+        self.isPicture = false;
+
+        // Question type
+        self.questionType = Math.random();
 
         // Initial values
         self.isDisplayedIcon = false;
         self.btn1 = "outline-dark";
         self.btn2 = "outline-dark";
-        
-        movie.getAll().$promise.then(function(data) {
-            
-            
-            
-            self.movies = data;    
-            self.directors = [];
 
-            for (var myMovie of self.movies) {
-                if (self.directors.filter(function(e) { return e == myMovie.director; }).length == 0) {
-                    self.directors.push(myMovie.director);
-                }
+        // Get all genres
+        category.getAll().$promise.then(function(data) {
+            self.categoryNames = self.getCategoryNames(data);
+        });
+
+        // Converts array of genre objects to array of genre names
+        self.getCategoryNames = function(categories) {
+            var categoriesNames = [];
+            for (var myCategory of categories) {
+                categoriesNames.push(myCategory.name);
             }
-            
-            // Get a random movie
-            self.movie = self.movies[Math.floor(Math.random()*self.movies.length)];
+            return categoriesNames;
+        };
 
-            // Get correct answer
-            self.correctAnswer = self.movie.director;
-
-            // The question
-            self.questionText = "Who directed '" + self.movie.title + "'?";
-            
-            // Find another random director
-            self.anotherAnswer = self.directors[Math.floor(Math.random()*self.directors.length)];
-            while (self.anotherAnswer == self.correctAnswer) {
-                self.anotherAnswer = self.directors[Math.floor(Math.random()*self.directors.length)];
+        // Converts array of actors objects to array of actors names
+        self.getActorNames = function(actors) {
+            var actorsNames = [];
+            for (var myActor of actors) {
+                actorsNames.push(myActor.first_name + " " + myActor.last_name);
             }
+            return actorsNames;
+        };
 
+        // Randomizes position of answers
+        self.randomizeAnswers = function(cor,fal) {
             if (Math.random()>0.5) {
-                self.answer1 = self.correctAnswer;
-                self.answer2 = self.anotherAnswer;
+                self.answer1 = cor;
+                self.answer2 = fal;
                 self.icon1 = "check";
                 self.icon2 = "close";
                 self.btn1Final = "success";
                 self.btn2Final = "danger";
             }
             else {
-                self.answer1 = self.anotherAnswer;
-                self.answer2 = self.correctAnswer;
+                self.answer1 = fal;
+                self.answer2 = cor;
                 self.icon1 = "close";
                 self.icon2 = "check";
                 self.btn1Final = "danger";
                 self.btn2Final = "success";
             }
+        };
 
-        });
+        if (self.questionType<0.66) {
 
-        $scope.displayIcon = function(chosenAnswer) {
+            movie.getAll().$promise.then(function(data) {
+                
+                // All the movies
+                self.movies = data;
+        
+                // All the movie directors
+                self.directors = [];
+                for (var myMovie of self.movies) {
+                    if (self.directors.filter(function(e) { return e == myMovie.director; }).length == 0) {
+                        self.directors.push(myMovie.director);
+                    }
+                }
+                    
+                // Some random movie
+                self.movie = self.movies[Math.floor(Math.random()*self.movies.length)];
+                    
+                if (self.questionType<0.33) {
+
+                    // Get correct answer (movie director)
+                    self.correctAnswer = self.movie.director;
+        
+                    // The question
+                    self.questionText = "Who directed '" + self.movie.title + "'?";
+                    
+                    // Find another random answer
+                    self.anotherAnswer = self.directors[Math.floor(Math.random()*self.directors.length)];
+                    while (self.anotherAnswer == self.correctAnswer) {
+                        self.anotherAnswer = self.directors[Math.floor(Math.random()*self.directors.length)];
+                    }
+
+                    // Randomize location of the correct answer
+                    self.randomizeAnswers(self.correctAnswer,self.anotherAnswer);
+
+                }
+
+                else {
+
+                    // Get movie's genres
+                    movie.getMovieCategories({movieId: self.movie.id}).$promise.then(function(cat) {
+                        
+                        self.movieCategories = self.getCategoryNames(cat);
+
+                        // Get correct answer (movie director)
+                        self.correctAnswer = self.movieCategories[Math.floor(Math.random()*self.movieCategories.length)];
+
+                        // The question
+                        self.questionText = "What genre is '" + self.movie.title + "'?";
+
+                        // Find another random answer
+                        //self.anotherAnswer = self.categoryNames[Math.floor(Math.random()*self.categoryNames.length)];
+                        self.anotherAnswer = self.correctAnswer;
+                        while (self.movieCategories.includes(self.anotherAnswer)) {
+                            self.anotherAnswer = self.categoryNames[Math.floor(Math.random()*self.categoryNames.length)];
+                        }
+
+                        // Randomize location of the correct answer
+                        self.randomizeAnswers(self.correctAnswer,self.anotherAnswer);
+
+                    });
+
+                }
+                    
+            });
+        }
+
+        else {
+
+            actor.getAll().$promise.then(function(data) {
+
+                // All the actors
+                self.actors = data;
+
+                self.actorsNames = self.getActorNames(self.actors);
+
+                // Some random actor
+                self.actor = self.actors[Math.floor(Math.random()*self.actors.length)];
+
+                // Get correct answer
+                self.correctAnswer = self.getActorNames([self.actor]).toString();
+
+                // The question
+                self.questionText = "Who is this?";
+
+                // Actor picture
+                self.actorPicture = self.actor.picture_url;
+                self.isPicture = true;
+                
+                // Find another random answer
+                self.anotherAnswer = self.actorsNames[Math.floor(Math.random()*self.actorsNames.length)];
+                while (self.anotherAnswer == self.correctAnswer) {
+                    self.anotherAnswer = self.actorsNames[Math.floor(Math.random()*self.actorsNames.length)];
+                }
+
+                // Randomize location of the correct answer
+                self.randomizeAnswers(self.correctAnswer,self.anotherAnswer);
+
+            });
+
+        }
+
+        // Answer selection
+        $scope.selectAnswer = function(chosenAnswer) {
             self.isDisplayedIcon = true;
             self.btn1 = self.btn1Final;
             self.btn2 = self.btn2Final;
 
             if (chosenAnswer == self.correctAnswer) {
+                self.score++;
                 self.ans = "Correct!";
             }
             else {
-                self.ans = "Incorrect!"
+                self.ans = "Incorrect!";
             }
 
-            // Your application has indicated there's an error
+            // Wait for 2 seconds and reload page
             window.setTimeout(function(){
-
-                // Move to a new location or you can do something else
                 window.location.href = "#!/quizz/";
-
             }, 2000);
         };
-
 
       }
     ]
